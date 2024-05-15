04837b3af4bb6c6cffac3736f12eac08ae369315
philanthropistaria <aatandoh56@gmail.com>
2024-05-15 17:27:58 +0000

    hi

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
